42620dd0e2985a0c6b8c36645ac35efa12783a11
Dylan <dylan.coben@gmail.com>
2024-05-09 13:03:53 -0400

    Added speed insights

--- node_portfolio_site/src/app/layout.tsx
+++ node_portfolio_site/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import React, { Suspense } from "react";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Analytics />
+      <SpeedInsights />
       <Suspense>
         <body className={inter.className}>{children}</body>
       </Suspense>
